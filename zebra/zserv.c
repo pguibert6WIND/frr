93847d4750fcfc9bc46d7799c101d977c8b11dfc
Philippe Guibert <philippe.guibert@6wind.com>
2018-03-27 11:27:10 +0200

    zebra: rework pbr ipset entry
    Add ns_id into zebra_pbr ipset
    This is important so that each ipset entry knows on which NETNS the
    ipset entry must be inkected
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zserv.c
+++ zebra/zserv.c
@@ -2836,6 +2836,7 @@ static inline void zread_ipset(ZAPI_HANDLER_ARGS)
 		memset(&zpi, 0, sizeof(zpi));
 
 		zpi.sock = client->sock;
+		zpi.vrf_id = zvrf->vrf->vrf_id;
 		STREAM_GETL(s, zpi.unique);
 		STREAM_GETL(s, zpi.type);
 		STREAM_GET(&zpi.ipset_name, s,
@@ -2907,6 +2908,7 @@ static inline void zread_iptable(ZAPI_HANDLER_ARGS)
 	memset(&zpi, 0, sizeof(zpi));
 
 	zpi.sock = client->sock;
+	zpi.vrf_id = zvrf->vrf->vrf_id;
 	STREAM_GETL(s, zpi.unique);
 	STREAM_GETL(s, zpi.type);
 	STREAM_GETL(s, zpi.filter_bm);
