pguibert6WIND/frr

commit 2e5c8240a56ba6a246e003df0828585612184692

bgpd: add missing ecommunity flowspec to display
On some cases, the ecommunity flowspec for redirect vrf is not displayed
in all cases. On top of that, display the values if ecom can no be
decoded.
Also, sub_type and type are changed from int to u_int8_t, because the
values contains match the type and sub type of extended communities.

PR=58846
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
Acked-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>

bgpd/bgp_ecommunity.c
@@ -637,8 +637,8 @@ char *ecommunity_ecom2str(struct ecommunity *ecom, int format, int filter)
 {
 	int i;
 	uint8_t *pnt;
-	int type = 0;
-	int sub_type = 0;
+	uint8_t type = 0;
+	uint8_t sub_type = 0;
 #define ECOMMUNITY_STR_DEFAULT_LEN  27
 	int str_size;
 	int str_pnt;
@@ -745,6 +745,21 @@ char *ecommunity_ecom2str(struct ecommunity *ecom, int format, int filter)
 					"FS:redirect IP 0x%x", *(pnt+5));
 			} else
 				unk_ecom = 1;
+		} else if ((sub_type == ECOMMUNITY_REDIRECT_VRF) &&
+			   (type == ECOMMUNITY_ENCODE_TRANS_EXP ||
+			    type == ECOMMUNITY_EXTENDED_COMMUNITY_PART_2 ||
+			    type == ECOMMUNITY_EXTENDED_COMMUNITY_PART_3)) {
+			char buf[16];
+
+			memset(buf, 0, sizeof(buf));
+			ecommunity_rt_soo_str(buf, (uint8_t *)pnt,
+					      type &
+					      ~ECOMMUNITY_ENCODE_TRANS_EXP,
+					      ECOMMUNITY_ROUTE_TARGET,
+					      ECOMMUNITY_FORMAT_DISPLAY);
+			len = snprintf(str_buf + str_pnt,
+				       str_size - len,
+				       "FS:redirect VRF %s", buf);
 		} else if (type == ECOMMUNITY_ENCODE_TRANS_EXP) {
 			sub_type = *pnt++;
 
@@ -777,30 +792,20 @@ char *ecommunity_ecom2str(struct ecommunity *ecom, int format, int filter)
 				len = sprintf(
 					str_buf + str_pnt,
 					"FS:rate %f", data.rate_float);
-			} else if (sub_type == ECOMMUNITY_REDIRECT_VRF) {
-				char buf[16];
-
-				memset(buf, 0, sizeof(buf));
-				ecommunity_rt_soo_str(buf, (uint8_t *)pnt,
-						type &
-						~ECOMMUNITY_ENCODE_TRANS_EXP,
-						ECOMMUNITY_ROUTE_TARGET,
-						ECOMMUNITY_FORMAT_DISPLAY);
-				len = snprintf(
-					str_buf + str_pnt,
-					str_size - len,
-					"FS:redirect VRF %s", buf);
 			} else if (sub_type == ECOMMUNITY_TRAFFIC_MARKING) {
 				len = sprintf(
 					str_buf + str_pnt,
 					"FS:marking %u", *(pnt+5));
 			} else
 				unk_ecom = 1;
-		} else
+		} else {
+			sub_type = *pnt++;
 			unk_ecom = 1;
+		}
 
 		if (unk_ecom)
-			len = sprintf(str_buf + str_pnt, "?");
+			len = sprintf(str_buf + str_pnt, "UNK:%d, %d",
+				      type, sub_type);
 
 		str_pnt += len;
 		first = 0;

commit 6e450adf77b2d87cafebf6f999f7d2dffd8c5217

bgpd: fix set shaper extended community flowspec to display
Since the set shaping was not displayed well, this commit handles the
fix.

Fixes: 2e5c8240a56b ("bgpd: add missing ecommunity flowspec to display")
PR=58846
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
Acked-by: Matthieu Ternisien d'Ouville <matthieu.tdo@6wind.com>

bgpd/bgp_ecommunity.c
@@ -761,12 +761,9 @@ char *ecommunity_ecom2str(struct ecommunity *ecom, int format, int filter)
 				len = snprintf(str_buf + str_pnt,
 					       str_size - len,
 					       "FS:redirect VRF %s", buf);
-			} else
+			} else if (type != ECOMMUNITY_ENCODE_TRANS_EXP)
 				unk_ecom = 1;
-		} else if (type == ECOMMUNITY_ENCODE_TRANS_EXP) {
-			sub_type = *pnt++;
-
-			if (sub_type == ECOMMUNITY_TRAFFIC_ACTION) {
+			else if (sub_type == ECOMMUNITY_TRAFFIC_ACTION) {
 				char action[64];
 				char *ptr = action;
 