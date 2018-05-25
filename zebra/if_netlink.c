pguibert6WIND/frr

commit b53686c52a592e5edf0d89f92ec9a2b83fc4e470

zebra: delete interface that disappeared
When moving interfaces to an other place, like other netns, the
remaining interface is still present, with inactive status.
Now, that interface is deleted from the list, if the interface appears
on an other netns. If not, the interface is kept.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/if_netlink.c
@@ -1008,6 +1008,54 @@ int netlink_interface_addr(struct sockaddr_nl *snl, struct nlmsghdr *h,
 	return 0;
 }
 
+/* helper function called by if_netlink_change
+ * to delete interfaces in case the interface moved
+ * to an other netns
+ */
+static void if_netlink_check_ifp_instance_consistency(uint16_t cmd,
+						     struct interface *ifp,
+						     ns_id_t ns_id)
+{
+	struct interface *old_ifp;
+
+	/*
+	 * look if interface name is also found on other netns
+	 * - only if vrf backend is netns
+	 * - do not concern lo interface
+	 * - then remove previous one
+	 * - for new link case, check found interface is not active
+	 */
+	if (!vrf_is_backend_netns() ||
+	    !strcmp(ifp->name, "lo"))
+		return;
+	old_ifp = if_lookup_by_name_not_ns(ns_id, ifp->name);
+	if (!old_ifp)
+		return;
+	if ((cmd == RTM_NEWLINK)
+	    && (CHECK_FLAG(old_ifp->status, ZEBRA_INTERFACE_ACTIVE)))
+		return;
+	if (IS_ZEBRA_DEBUG_KERNEL)
+		zlog_debug("%s %s(%u) %s VRF %u",
+			   cmd == RTM_DELLINK ?
+			   "RTM_DELLINK replaced by" :
+			   "RTM_NEWLINK replaces",
+			   ifp->name,
+			   old_ifp->ifindex,
+			   cmd == RTM_DELLINK ?
+			   "in" : "from",
+			   old_ifp->vrf_id);
+	/* the found interface replaces the current one
+	 * remove it
+	 */
+	if (cmd == RTM_DELLINK)
+		if_delete(ifp);
+	else
+		if_delete(old_ifp);
+	/* the found interface is replaced by the current one
+	 * suppress it
+	 */
+}
+
 int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			ns_id_t ns_id, int startup)
 {
@@ -1169,6 +1217,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp))
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
+			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
+								  ifp, ns_id);
 		} else if (ifp->vrf_id != vrf_id) {
 			/* VRF change for an interface. */
 			if (IS_ZEBRA_DEBUG_KERNEL)
@@ -1236,6 +1286,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 			if (IS_ZEBRA_IF_BRIDGE_SLAVE(ifp) || was_bridge_slave)
 				zebra_l2if_update_bridge_slave(ifp,
 							       bridge_ifindex);
+			if_netlink_check_ifp_instance_consistency(RTM_NEWLINK,
+								  ifp, ns_id);
 		}
 	} else {
 		/* Delete interface notification from kernel */
@@ -1259,6 +1311,8 @@ int netlink_link_change(struct sockaddr_nl *snl, struct nlmsghdr *h,
 
 		if (!IS_ZEBRA_IF_VRF(ifp))
 			if_delete_update(ifp);
+		if_netlink_check_ifp_instance_consistency(RTM_DELLINK,
+							  ifp, ns_id);
 	}
 
 	return 0;

zebra/interface.c
@@ -252,6 +252,30 @@ struct interface *if_lookup_by_name_per_ns(struct zebra_ns *ns,
 	return NULL;
 }
 
+/* this function must be used only if the vrf backend
+ * is a netns backend
+ */
+struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
+					   const char *ifname)
+{
+	struct interface *ifp;
+	struct ns *ns;
+
+	RB_FOREACH (ns, ns_head, &ns_tree) {
+		if (ns->ns_id == ns_id)
+			continue;
+		/* if_delete_update has removed interface
+		 * from zns->if_table
+		 * so to look for interface, use the vrf list
+		 */
+		ifp = if_lookup_by_name(ifname, (vrf_id_t)ns->ns_id);
+		if (!ifp)
+			continue;
+		return ifp;
+	}
+	return NULL;
+}
+
 const char *ifindex2ifname_per_ns(struct zebra_ns *zns, unsigned int ifindex)
 {
 	struct interface *ifp;

zebra/interface.h
@@ -318,6 +318,8 @@ extern struct interface *if_lookup_by_index_per_ns(struct zebra_ns *,
 						   u_int32_t);
 extern struct interface *if_lookup_by_name_per_ns(struct zebra_ns *,
 						  const char *);
+extern struct interface *if_lookup_by_name_not_ns(ns_id_t ns_id,
+						  const char *ifname);
 extern struct interface *if_link_per_ns(struct zebra_ns *, struct interface *);
 extern const char *ifindex2ifname_per_ns(struct zebra_ns *, unsigned int);
 

commit c57cea42f4e7f686cd854b8690968c0be16adb1b

zebra: fix incorrect if deletion when an interface returns to vr0
When an interface is moved from VRx to VR0 the following netlink sequence
can occur:
1. Creation of the interface in the VR0
2. Deletion of the interface in the VRx

In this case on the reception of the deletion message the code does:
a. Call the if_delete_update function (in zebra/interface.c) to set the
moved interface on VRx as inactive and clean information relative to
this if.
b. Call the if_handle_vrf_change function (in zebra/interface.c) to move
the interface to VR0 to allow vr deletion in the future. This operation:
   - logs the following message "corruption detected -- interface with this
   name exists already in VRF 0!" as we receive previously the netlink
   creation message of this interface in VR0
   - set the vrf_id for this if to VR0
c. Call the if_netlink_check_ifp_instance_consistency function (in
zebra/if_netlink.c) in order check the ifp consistency. This operation:
   - find a VR (here VR0) where the interface is also present
   - remove the interface from the VRx but as the vr_id of the interface
   has been previously change the interface is removed on VR0 and not
   VRx

Then zebra internal interface state is inconsistent and future behaviour
is unpredictable (opeation failure or crash can occur)

To solve this issue the vrid of the interface to remove is reset to vrx
to be sure to remove the expected interface. The warning message is
still present but the zebra internal interface state is always correct.

Fixes: b53686c52a59 ("zebra: delete interface that disappeared")
PR=58846
Signed-off-by: Thibaut Collet <thibaut.collet@6wind.com>
Acked-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/if_netlink.c
@@ -1061,7 +1061,7 @@ static void if_netlink_check_ifp_instance_consistency(uint16_t cmd,
 		zlog_debug("RTM_DELLINK %s(%u, VRF %u) is replaced by %s(%u, VRF %u)\n",
 			   ifp->name,
 			   ifp->ifindex,
-			   ifp->vrf_id,
+			   ns_id,
 			   other_ifp->name,
 			   other_ifp->ifindex,
 			   other_ifp->vrf_id);
@@ -1069,9 +1069,10 @@ static void if_netlink_check_ifp_instance_consistency(uint16_t cmd,
 	/* the found interface replaces the current one
 	 * remove it
 	 */
-	if (cmd == RTM_DELLINK)
+	if (cmd == RTM_DELLINK) {
+		ifp->vrf_id = (vrf_id_t)ns_id;
 		if_delete(ifp);
-	else
+	} else
 		if_delete(other_ifp);
 	/* the found interface is replaced by the current one
 	 * suppress it