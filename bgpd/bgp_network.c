5ada8e3228f5203dde7d49780b95885aed82ffc4
Donald Sharp <sharpd@cumulusnetworks.com>
2018-09-06 10:51:08 -0400

    bgpd: Prevent possible crash when parsing v6 attributes
    The peer->nexthop.ifp pointer must be set when parsing the
    attributes in bgp_mp_reach_parse, notice this
    and fail gracefully.
    
    Rework bgp_nexthop_set to remove the HAVE_CUMULUS and to
    fail the nexthop_set when we have a zebra connection and
    no ifp pointer, as that not havinga zebra connection and
    no ifp pointer is legal.
    
    PR=61154
    Signed-off-by: Donald Sharp <sharpd@cumulusnetworks.com>
    (cherry picked from commit 17cdd31e00b2ca2763c042a415bfcf03b614a783)
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Alain Ritoux <alain.ritoux@6wind.com>

--- bgpd/bgp_network.c
+++ bgpd/bgp_network.c
@@ -35,6 +35,7 @@
 #include "hash.h"
 #include "filter.h"
 #include "ns.h"
+#include "nexthop.h"
 
 #include "bgpd/bgpd.h"
 #include "bgpd/bgp_open.h"
@@ -42,6 +43,7 @@
 #include "bgpd/bgp_attr.h"
 #include "bgpd/bgp_debug.h"
 #include "bgpd/bgp_network.h"
+#include "bgpd/bgp_zebra.h"
 
 extern struct zebra_privs_t bgpd_privs;
 
@@ -620,14 +622,11 @@ int bgp_getsockname(struct peer *peer)
 	if (!peer->su_remote)
 		return -1;
 
-	if (bgp_nexthop_set(peer->su_local, peer->su_remote, &peer->nexthop,
-			    peer)) {
-#if defined(HAVE_CUMULUS)
-		zlog_err(
-			"%s: nexthop_set failed, resetting connection - intf %p",
-			peer->host, peer->nexthop.ifp);
+	if (!bgp_zebra_nexthop_set(peer->su_local, peer->su_remote,
+				   &peer->nexthop, peer)) {
+		zlog_err("%s: nexthop_set failed, resetting connection - intf %p",
+			 peer->host, peer->nexthop.ifp);
 		return -1;
-#endif
 	}
 	return 0;
 }
