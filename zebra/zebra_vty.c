pguibert6WIND/frr

commit b9898edb3d2dfdbd13bb962fc6035c21ff96a714

zebra: add show ip route table vrf command
It is possible to get table per VRF, provided that vrf backend is
configured with NETNS.

Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/zebra_vty.c
@@ -1880,6 +1880,35 @@ DEFPY (show_route_table,
 	return CMD_SUCCESS;
 }
 
+DEFPY (show_route_table_vrf,
+       show_route_table_vrf_cmd,
+       "show <ip$ipv4|ipv6$ipv6> route table (1-4294967295)$table vrf NAME$vrf_name [json$json]",
+       SHOW_STR
+       IP_STR
+       IP6_STR
+       "IP routing table\n"
+       "Table to display\n"
+       "The table number to display, if available\n"
+       VRF_CMD_HELP_STR
+       JSON_STR)
+{
+	afi_t afi = ipv4 ? AFI_IP : AFI_IP6;
+	struct zebra_vrf *zvrf;
+	struct route_table *t;
+	vrf_id_t vrf_id = VRF_DEFAULT;
+
+	if (vrf_name)
+		VRF_GET_ID(vrf_id, vrf_name);
+	zvrf = zebra_vrf_lookup_by_id(vrf_id);
+
+	t = zebra_ns_find_table(zvrf->zns, table, afi);
+	if (t)
+		do_show_route_helper(vty, zvrf, t, afi, false, 0, false, false,
+				     0, 0, !!json);
+
+	return CMD_SUCCESS;
+}
+
 DEFUN (show_ip_nht,
        show_ip_nht_cmd,
        "show ip nht [vrf NAME]",
@@ -4094,6 +4123,8 @@ void zebra_vty_init(void)
 	install_element(VIEW_NODE, &show_vrf_vni_cmd);
 	install_element(VIEW_NODE, &show_route_cmd);
 	install_element(VIEW_NODE, &show_route_table_cmd);
+	if (vrf_is_backend_netns())
+		install_element(VIEW_NODE, &show_route_table_vrf_cmd);
 	install_element(VIEW_NODE, &show_route_detail_cmd);
 	install_element(VIEW_NODE, &show_route_summary_cmd);
 	install_element(VIEW_NODE, &show_ip_nht_cmd);

commit 980e1c3fd95a29e504307503c1888d0a29320f90

zebra: add show ip route vrf table command
To display route of table XXX of the vrf YYY only the following command is
supported:
vtysh -c "show ip route table XXX vrf YYY"

As the vrf and table parameters ordering is not important add the
support of the command:
vtysh -c "show ip route vrf YYY table XXX"

Fixes: b9898edb3d2d ("zebra: add show ip route table vrf command")
PR=60342
Signed-off-by: Thibaut Collet <thibaut.collet@6wind.com>
Acked-by: Alain Ritoux <alain.ritoux@6wind.com>

zebra/zebra_vty.c
@@ -1909,6 +1909,35 @@ DEFPY (show_route_table_vrf,
 	return CMD_SUCCESS;
 }
 
+DEFPY (show_route_vrf_table,
+       show_route_vrf_table_cmd,
+       "show <ip$ipv4|ipv6$ipv6> route vrf NAME$vrf_name table (1-4294967295)$table [json$json]",
+       SHOW_STR
+       IP_STR
+       IP6_STR
+       VRF_CMD_HELP_STR
+       "IP routing table\n"
+       "Table to display\n"
+       "The table number to display, if available\n"
+       JSON_STR)
+{
+	afi_t afi = ipv4 ? AFI_IP : AFI_IP6;
+	struct zebra_vrf *zvrf;
+	struct route_table *t;
+	vrf_id_t vrf_id = VRF_DEFAULT;
+
+	if (vrf_name)
+		VRF_GET_ID(vrf_id, vrf_name);
+	zvrf = zebra_vrf_lookup_by_id(vrf_id);
+
+	t = zebra_ns_find_table(zvrf->zns, table, afi);
+	if (t)
+		do_show_route_helper(vty, zvrf, t, afi, false, 0, false, false,
+				     0, 0, !!json);
+
+	return CMD_SUCCESS;
+}
+
 DEFUN (show_ip_nht,
        show_ip_nht_cmd,
        "show ip nht [vrf NAME]",
@@ -4134,8 +4163,10 @@ void zebra_vty_init(void)
 	install_element(VIEW_NODE, &show_vrf_vni_cmd);
 	install_element(VIEW_NODE, &show_route_cmd);
 	install_element(VIEW_NODE, &show_route_table_cmd);
-	if (vrf_is_backend_netns())
+	if (vrf_is_backend_netns()) {
 		install_element(VIEW_NODE, &show_route_table_vrf_cmd);
+		install_element(VIEW_NODE, &show_route_vrf_table_cmd);
+	}
 	install_element(VIEW_NODE, &show_route_detail_cmd);
 	install_element(VIEW_NODE, &show_route_summary_cmd);
 	install_element(VIEW_NODE, &show_ip_nht_cmd);