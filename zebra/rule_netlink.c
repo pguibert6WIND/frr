6a3fc7bf71fdf4152a07305fdf9daa121fa2b66b
Philippe Guibert <philippe.guibert@6wind.com>
2018-11-29 15:22:27 +0100

    zebra: support for iprule with from <any> to <any>
    enhance ip rule configuration for default routes too, when no interface
    ( no pbr) and no fwmark ( the other kind of object handled by bgp) is
    not supported.
    
    PR=61620
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Alain Ritoux <alain.ritoux@6wind.com>

--- zebra/rule_netlink.c
+++ zebra/rule_netlink.c
@@ -66,7 +66,10 @@ static int netlink_rule_update(int cmd, struct zebra_pbr_rule *rule)
 	char buf2[PREFIX_STRLEN];
 
 	memset(&req, 0, sizeof(req) - NL_PKT_BUF_SIZE);
-	family = PREFIX_FAMILY(&rule->rule.filter.src_ip);
+	if (IS_RULE_FILTERING_ON_SRC_IP(rule))
+		family = PREFIX_FAMILY(&rule->rule.filter.src_ip);
+	else if (IS_RULE_FILTERING_ON_DST_IP(rule))
+		family = PREFIX_FAMILY(&rule->rule.filter.dst_ip);
 	bytelen = (family == AF_INET ? 4 : 16);
 
 	req.n.nlmsg_type = cmd;
@@ -115,10 +118,11 @@ static int netlink_rule_update(int cmd, struct zebra_pbr_rule *rule)
 
 	if (IS_ZEBRA_DEBUG_KERNEL)
 		zlog_debug(
-			"Tx %s family %s IF %s(%u) Pref %u Src %s Dst %s Table %u",
+			"Tx %s family %s IF %s(%u) Pref %u Fwmark %u Src %s Dst %s Table %u",
 			nl_msg_type_to_str(cmd), nl_family_to_str(family),
 			rule->ifp ? rule->ifp->name : "Unknown",
 			rule->ifp ? rule->ifp->ifindex : 0, rule->rule.priority,
+			rule->rule.filter.fwmark,
 			prefix2str(&rule->rule.filter.src_ip, buf1,
 				   sizeof(buf1)),
 			prefix2str(&rule->rule.filter.dst_ip, buf2,
