b953ca9d0fa5b4dc3635e533e3d914059769d586
vivek <vivek@cumulusnetworks.com>
2020-05-15 14:01:59 -0700

    zebra: Install bridge FDB entries with correct VTEP IP
    Fixes: zebra: adapt and export rmac netlink functions
    
    Signed-off-by: Vivek Venkatraman <vivek@cumulusnetworks.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -2610,10 +2610,15 @@ int kernel_neigh_update(int add, int ifindex, uint32_t addr, char *lla,
 /**
  * netlink_update_neigh_ctx_internal() - Common helper api for evpn
  * neighbor updates using dataplane context object.
+ * Here, a neighbor refers to a bridge forwarding database entry for
+ * either unicast forwarding or head-end replication or an IP neighbor
+ * entry.
  * @ctx:		Dataplane context
  * @cmd:		Netlink command (RTM_NEWNEIGH or RTM_DELNEIGH)
  * @mac:		A neighbor cache link layer address
  * @ip:		A neighbor cache n/w layer destination address
+ *			In the case of bridge FDB, this represnts the remote
+ *			VTEP IP.
  * @replace_obj:	Whether NEW request should replace existing object or
  *			add to the end of the list
  * @family:		AF_* netlink family
@@ -3146,7 +3151,7 @@ netlink_macfdb_update_ctx(struct zebra_dplane_ctx *ctx, uint8_t *data,
 
 	total = netlink_update_neigh_ctx_internal(
 			ctx, cmd, dplane_ctx_mac_get_addr(ctx),
-			dplane_ctx_neigh_get_ipaddr(ctx), true, AF_BRIDGE, 0,
+			&vtep_ip, true, AF_BRIDGE, 0,
 			flags, state, nhg_id, nfy, nfy_flags,
 			false /*ext*/, 0 /*ext_flags*/,
 			data, datalen);
