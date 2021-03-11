7b6dd0c3091605aadcad4b3f143eeafe4d287f71
Philippe Guibert <philippe.guibert@6wind.com>
2021-03-11 15:33:41 +0100

    zebra: new dplane action to set gre link interface
    This action is initiated by nhrp and has been stubbed when
    moving to zebra. Now, a netlink request is forged to set
    the link interface of a gre interface if that gre interface
    does not have already a link interface.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/if_netlink.c
+++ zebra/if_netlink.c
@@ -464,6 +464,46 @@ uint32_t kernel_get_speed(struct interface *ifp, int *error)
 	return get_iflink_speed(ifp, error);
 }
 
+static ssize_t netlink_gre_set_msg_encoder(struct zebra_dplane_ctx *ctx, void *buf,
+					   size_t buflen)
+{
+	struct {
+		struct nlmsghdr n;
+		struct ifinfomsg ifi;
+		char buf[];
+	} *req = buf;
+	uint32_t link_idx;
+	struct rtattr *rta_info, *rta_data;
+
+	if (buflen < sizeof(*req))
+		return 0;
+	memset(req, 0, sizeof(*req));
+
+	req->n.nlmsg_type =  RTM_NEWLINK;
+	req->n.nlmsg_len = NLMSG_LENGTH(sizeof(struct ifinfomsg));
+	req->n.nlmsg_flags = NLM_F_REQUEST;
+
+	memset(&req->ifi, 0, sizeof(struct ifinfomsg));
+	req->ifi.ifi_index = dplane_ctx_get_ifindex(ctx);
+	req->ifi.ifi_change = 0xFFFFFFFF;
+	link_idx = dplane_ctx_gre_get_link_ifindex(ctx);
+
+	rta_info = nl_attr_nest(&req->n, buflen, IFLA_LINKINFO);
+	if (!rta_info)
+		return 0;
+	if (!nl_attr_put(&req->n, buflen, IFLA_INFO_KIND, "gre", 3))
+		return 0;
+	rta_data = nl_attr_nest(&req->n, buflen, IFLA_INFO_DATA);
+	if (!rta_info)
+		return 0;
+	if (!nl_attr_put32(&req->n, buflen, IFLA_GRE_LINK, link_idx))
+		return 0;
+	nl_attr_nest_end(&req->n, rta_data);
+	nl_attr_nest_end(&req->n, rta_info);
+
+	return NLMSG_ALIGN(req->n.nlmsg_len);
+}
+
 static int netlink_extract_bridge_info(struct rtattr *link_data,
 				       struct zebra_l2info_bridge *bridge_info)
 {
@@ -535,6 +575,8 @@ static int netlink_extract_gre_info(struct rtattr *link_data,
 		ifindex_link =
 			*(ifindex_t *)RTA_DATA(attr[IFLA_GRE_LINK]);
 		gre_info->ifindex_link = ifindex_link;
+		if (IS_ZEBRA_DEBUG_KERNEL)
+			zlog_debug("IFLA_GRE_LINK obtained is %u", ifindex_link);
 	}
 	if (attr[IFLA_GRE_IKEY])
 		gre_info->ikey = *(uint32_t *)RTA_DATA(attr[IFLA_GRE_IKEY]);
@@ -993,6 +1035,26 @@ static int netlink_request_intf_addr(struct nlsock *netlink_cmd, int family,
 		nl_attr_put32(&req.n, sizeof(req), IFLA_EXT_MASK, filter_mask);
 
 	return netlink_request(netlink_cmd, &req);
+}
+
+enum netlink_msg_status
+netlink_put_gre_set_msg(struct nl_batch *bth, struct zebra_dplane_ctx *ctx)
+{
+	enum dplane_op_e op;
+	enum netlink_msg_status ret;
+
+	op = dplane_ctx_get_op(ctx);
+	if (!(op == DPLANE_OP_GRE_SET)) {
+		flog_err(
+			EC_ZEBRA_GRE_SET_UPDATE,
+			"Context received for kernel gre set update with incorrect OP code (%u)",
+			op);
+		return FRR_NETLINK_ERROR;
+	}
+
+	ret = netlink_batch_add_msg(bth, ctx, netlink_gre_set_msg_encoder, false);
+
+	return ret;
 }
 
 /* Interface lookup by netlink socket. */
