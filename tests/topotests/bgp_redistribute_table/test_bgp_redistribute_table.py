8f28a0b4d50f8f6fc7b0e47e8b5401d06fc2a804
Philippe Guibert <philippe.guibert@6wind.com>
2023-08-28 11:12:20 +0200

    topotests: add bgp_redistribute_table test
    There is no test that ensures the test of the 'redistribute
    table-direct' facility. Add a test that checks that routes
    created before and after BGP is started, is correctly imported.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

---
+++ tests/topotests/bgp_redistribute_table/test_bgp_redistribute_table.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: ISC
+
+#
+# test_bgp_redistribute_table.py
+# Part of NetDEF Topology Tests
+#
+# Copyright (c) 2022 by 6WIND
+#
+
+"""
+ test_bgp_redistribute_table.py: Test the FRR BGP daemon with 'redistribute direct-table'
+"""
+
+import os
+import sys
+import json
+from functools import partial
+import pytest
+
+# Save the Current Working Directory to find configuration files.
+CWD = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(CWD, "../"))
+
+# pylint: disable=C0413
+# Import topogen and topotest helpers
+from lib import topotest
+from lib.topogen import Topogen, TopoRouter, get_topogen
+from lib.topolog import logger
+
+# Required to instantiate the topology builder class.
+
+
+pytestmark = [pytest.mark.bgpd]
+
+
+def build_topo(tgen):
+    "Build function"
+
+    # Create 2 routers.
+    tgen.add_router("r1")
+    tgen.add_router("r2")
+
+    switch = tgen.add_switch("s1")
+    switch.add_link(tgen.gears["r1"])
+    switch.add_link(tgen.gears["r2"])
+
+    switch = tgen.add_switch("s2")
+    switch.add_link(tgen.gears["r1"])
+
+    switch = tgen.add_switch("s3")
+    switch.add_link(tgen.gears["r2"])
+
+
+def setup_module(mod):
+    "Sets up the pytest environment"
+    tgen = Topogen(build_topo, mod.__name__)
+    tgen.start_topology()
+
+    router_list = tgen.routers()
+
+    for rname, router in router_list.items():
+        router.load_config(
+            TopoRouter.RD_ZEBRA, os.path.join(CWD, "{}/zebra.conf".format(rname))
+        )
+        router.load_config(
+            TopoRouter.RD_BGP, os.path.join(CWD, "{}/bgpd.conf".format(rname))
+        )
+
+    # Initialize all routers.
+    tgen.start_router()
+
+
+def teardown_module(_mod):
+    "Teardown the pytest environment"
+    tgen = get_topogen()
+
+    tgen.stop_topology()
+
+
+def test_protocols_convergence():
+    """
+    Assert that all protocols have converged
+    statuses as they depend on it.
+    """
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    router = tgen.gears["r1"]
+
+    # Check IPv4 routing tables on r1
+    logger.info("Checking IPv4 routes for convergence on r1")
+    router = tgen.gears["r1"]
+    json_file = "{}/{}/ipv4_routes.json".format(CWD, router.name)
+    expected = json.loads(open(json_file).read())
+    test_func = partial(
+        topotest.router_json_cmp,
+        router,
+        "show ip route bgp json",
+        expected,
+    )
+    _, result = topotest.run_and_expect(test_func, None, count=10, wait=0.5)
+    assertmsg = '"{}" JSON output mismatches'.format(router.name)
+    assert result is None, assertmsg
+
+
+def test_stop_bgp_and_add_kernel_route_and_restart_bgp():
+    "Sets up the pytest environment"
+    tgen = get_topogen()
+    if tgen.routers_have_failure():
+        pytest.skip(tgen.errors)
+
+    router_list = tgen.routers()
+
+    router = tgen.gears["r2"]
+    logger.info("Removing r2 BGP configuration")
+    router.vtysh_cmd("configure terminal\nno router bgp 65501\n")
+
+    cmd = "ip route add 172.31.0.15/32 via 172.31.1.100 table 220"
+    tgen.net["r2"].cmd(cmd)
+
+    router = tgen.gears["r2"]
+    logger.info("Restoring r2 BGP configuration")
+    tgen.net["r2"].cmd("vtysh -f {}".format(os.path.join(CWD, "r2/bgpd.conf")))
+
+    # Check IPv4 routing tables on r1
+    logger.info("Checking IPv4 routes for convergence on r1 with kernel route")
+    router = tgen.gears["r1"]
+    json_file = "{}/{}/ipv4_routes_post.json".format(CWD, router.name)
+    expected = json.loads(open(json_file).read())
+    test_func = partial(
+        topotest.router_json_cmp,
+        router,
+        "show ip route bgp json",
+        expected,
+    )
+    _, result = topotest.run_and_expect(test_func, None, count=10, wait=0.5)
+    assertmsg = '"{}" JSON output mismatches'.format(router.name)
+    assert result is None, assertmsg
+
+
+def test_memory_leak():
+    "Run the memory leak test and report results."
+    tgen = get_topogen()
+    if not tgen.is_memleak_enabled():
+        pytest.skip("Memory leak test/report is disabled")
+
+    tgen.report_memory_leaks()
+
+
+if __name__ == "__main__":
+    args = ["-s"] + sys.argv[1:]
+    sys.exit(pytest.main(args))
