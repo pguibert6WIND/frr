b0290423782399c0b0471214fd45b586aa99120e
Philippe Guibert <philippe.guibert@6wind.com>
2024-01-05 16:55:24 +0100

    bgpd: add nhg group support for multiple path routes
    To control multiple paths with nexthop groups, a group
    dependency between nexthop groups must be set at the protocol
    level.
    
    Reuse the Nexthop Group TYPE_GROUP option.
    For each single path to install, two nexthop groups will be used.
    
    - Link the parent nexthop group to the path (with pi->bgp_nhg)
    - Link the original bgp_nhg_cache nexthop to the path (with pi->bgp_nhg_nexthop).
    - Handle the installation order at proto level: the nexthop first, the parent after
    - Add support in 'show bgp nexthop-group'
    - unlock the ability to handle multiple paths with NHGs
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- bgpd/bgp_nhg.h
+++ bgpd/bgp_nhg.h
@@ -12,9 +12,20 @@
 
 PREDECL_HASH(bgp_nhg_cache);
 PREDECL_RBTREE_UNIQ(bgp_nhg_cache_id);
+PREDECL_RBTREE_UNIQ(bgp_nhg_connected_tree);
 
 extern struct bgp_nhg_cache_head nhg_cache_table;
 extern struct bgp_nhg_cache_id_head nhg_cache_id_table;
+
+struct bgp_nhg_nexthop_cache {
+	uint16_t nexthop_num;
+	struct zapi_nexthop nexthops[MULTIPATH_NUM];
+};
+
+struct bgp_nhg_group_cache {
+	uint16_t group_num;
+	uint32_t groups[MULTIPATH_NUM];
+};
 
 struct bgp_nhg_cache {
 	struct bgp_nhg_cache_item entry;
@@ -29,22 +40,43 @@ struct bgp_nhg_cache {
 #define BGP_NHG_FLAG_ALLOW_RECURSION (1 << 0)
 #define BGP_NHG_FLAG_IBGP	     (1 << 1)
 #define BGP_NHG_FLAG_SRTE_PRESENCE   (1 << 2)
+#define BGP_NHG_FLAG_TYPE_GROUP	     (1 << 3)
 	uint16_t flags;
 #define BGP_NHG_STATE_INSTALLED (1 << 0)
 #define BGP_NHG_STATE_REMOVED	(1 << 1)
+#define BGP_NHG_STATE_MODIFIED	(1 << 2)
 	uint16_t state;
 
 	/* other parameters are route attributes and are not
 	 * relevant for qualifying next-hop:
 	 * tag, metric, distance
 	 */
-	uint16_t nexthop_num;
-	struct zapi_nexthop nexthops[MULTIPATH_NUM];
+	union {
+		struct bgp_nhg_nexthop_cache nexthops;
+		struct bgp_nhg_group_cache groups;
+	};
 
 	LIST_HEAD(nhg_path_list, bgp_path_info) paths;
 
 	unsigned int path_count;
 	time_t last_update;
+
+	/* list of groups:
+	 * For instance a group with two
+	 * nexthops will have two dependencies
+	 * pointing to those bgp_nhg.
+	 *
+	 * NHG id 3 with nexthops id 1/2
+	 * bgp_nhg(3)->depends has 1 and 2 in the tree
+	 * bgp_nhg(3)->dependents_thread is empty
+	 *
+	 * bgp_nhg(1)->depends is empty
+	 * bgp_nhg(1)->dependents_thread is 3 in the tree
+	 *
+	 * bgp_nhg(2)->depends is empty
+	 * bgp_nhg(2)->dependents_thread is 3 in the tree
+	 */
+	struct bgp_nhg_connected_tree_head nhg_depends, nhg_dependents;
 };
 
 extern uint32_t bgp_nhg_cache_hash(const struct bgp_nhg_cache *nhg);
@@ -67,12 +99,21 @@ extern struct bgp_nhg_cache *bgp_nhg_find(struct bgp *bgp, struct bgp_dest *dest
 					  struct bgp_path_info *pi, afi_t afi,
 					  safi_t safi);
 extern void bgp_nhg_path_unlink(struct bgp_path_info *pi);
+extern void bgp_nhg_path_nexthop_unlink(struct bgp_path_info *pi, bool force);
+extern void bgp_nhg_del_nhg(struct bgp_nhg_cache *nhg);
+extern void bgp_nhg_group_link(struct bgp_nhg_cache *nhg[], int nexthop_num,
+			       struct bgp_nhg_cache *nhg_parent);
 
-extern struct bgp_nhg_cache *bgp_nhg_new(uint32_t flags, uint16_t nexthop_num,
-					 struct zapi_nexthop api_nh[]);
+extern struct bgp_nhg_cache *bgp_nhg_new(uint32_t flags, uint16_t num,
+					 struct zapi_nexthop api_nh[],
+					 uint32_t api_group[]);
 extern void bgp_nhg_id_set_installed(uint32_t id, bool install);
 extern void bgp_nhg_id_set_removed(uint32_t id);
 extern void bgp_nhg_refresh_by_nexthop(struct bgp_nexthop_cache *bnc);
 void bgp_nhg_vty_init(void);
+void bgp_nhg_group_sort(uint32_t grp[], uint16_t nhg_num);
+void bgp_nhg_clear_nhg_nexthop(void);
+void bgp_nhg_debug_group(uint32_t api_groups[], int count, char *group_buf,
+			 size_t len);
 
 #endif /* _BGP_NHG_H */
