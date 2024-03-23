f6200171418f30e95426d53a74ee4a69ad3a5eb7
Dmytro Shytyi <dmytro.shytyi@6wind.com>
2024-03-23 03:52:02 +0100

    lib: add missing SR-TE to ipv6 route show command
    Add "p - SR-TE" field to the list.
    
    rt2# show ipv6 route
    Codes: K - kernel route, C - connected, L - local, S - static,
           R - RIPng, O - OSPFv3, I - IS-IS, B - BGP, N - NHRP,
           T - Table, v - VNC, V - VNC-Direct, A - Babel, D - SHARP,
           F - PBR, f - OpenFabric, t - Table-Direct, p - SR-TE
           > - selected route, * - FIB route, q - queued, r - rejected, b - backup
           t - trapped, o - offload failure
    
    Signed-off-by: Dmytro Shytyi <dmytro.shytyi@6wind.com>

--- lib/route_types.pl
+++ lib/route_types.pl
@@ -115,7 +115,8 @@ sub codelist {
 		$str .= $s;
 	}
 	$str =~ s/ $//;
-	push @lines, $str . "\\n\" \\\n";
+	push @lines, $str . " p - SR-TE,";
+	push @lines, "\\n\" \\\n";
 	push @lines, "  \"       > - selected route, * - FIB route, q - queued, r - rejected, b - backup\\n\"";
 	push @lines, "  \"       t - trapped, o - offload failure\\n\\n\"";
 
@@ -136,7 +137,7 @@ sub collect {
 		next if ($ip_prot == 1 && $daemon eq "zebra" && $protodetail{$p}->{"cname"} eq "connected");
 		next if ($ip_prot == 1 && $daemon eq "zebra" && $protodetail{$p}->{"cname"} eq "local");
 		next if ($protodetail{$p}->{"daemon"} eq $daemon && $daemon ne "zebra");
-		next if ($protodetail{$p}->{"restrict2"} ne "" && 
+		next if ($protodetail{$p}->{"restrict2"} ne "" &&
 		         $protodetail{$p}->{"restrict2"} ne $daemon);
 		next if ($protodetail{$p}->{"redist"} eq 0);
 		next unless (grep $_ eq $protodetail{$p}->{"enabled"}, @enabled);
@@ -215,4 +216,3 @@ print <<EOF;
 
 #endif /* _FRR_ROUTE_TYPES_H */
 EOF
-
