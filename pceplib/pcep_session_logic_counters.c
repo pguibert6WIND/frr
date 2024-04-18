df5da624961f5ffb72f1ab462374121689a3132d
Philippe Guibert <philippe.guibert@6wind.com>
2024-04-18 18:15:16 +0200

    pathd, pceplib, doc: add support for pcep SRv6-PCE-CAPABILITY tlv
    Add the SRv6 PCE capability option number 27 as defined per
    rfc9603.
    
    Link: http://datatracker.ietf.org/doc/html/rfc9603
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- pceplib/pcep_session_logic_counters.c
+++ pceplib/pcep_session_logic_counters.c
@@ -248,6 +248,8 @@ void create_session_counters(pcep_session *session)
 	create_subgroup_counter(rx_tlv_subgroup,
 				PCEP_OBJ_TLV_TYPE_SR_PCE_CAPABILITY,
 				"TLV SR PCE Capability", "TLVSRPCECapability");
+	create_subgroup_counter(rx_tlv_subgroup, PCEP_OBJ_TLV_TYPE_SRV6_PCE_CAPABILITY,
+				"TLV SRv6 PCE Capability", "TLVSRV6PCECapability");
 	create_subgroup_counter(rx_tlv_subgroup,
 				PCEP_OBJ_TLV_TYPE_PATH_SETUP_TYPE,
 				"TLV Path Setup Type", "TLVPathSetupType");
