88a485e08ecb6eee61330a169cad99eedb588ad6
Philippe Guibert <philippe.guibert@6wind.com>
2024-06-11 10:19:19 +0200

    pathd: send ROUTE_DEL message to ZEBRA when BSID IPv6 is not installed
    The update of the srv6-binding-sid value of a policy triggers the
    emission of a message to ZEBRA
    
    configured:
    > segment-routing
    > traffic-eng
    > policy color 1 endpoint 1::1
    > srv6-binding-sid 1001::1
    > [..]
    > srv6-binding-sid 1001::40
    
    output on logs:
    > 2024/06/11 10:09:35 ZEBRA: [YXG8K-BCYMV] zebra message[ZEBRA_ROUTE_DELETE:0:40] comes from socket [34]
    
    The previous BSID is attempted to be flushed, whereas it has never
    been installed: the SR policy is not up and does not have any
    candidate paths.
    
    Fix this by using a BSID_IPV6_INSTALLED flag on srte policy to
    tell whether the BSID is installed or not.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>
    Acked-by: Dmytro Shytyi <dmytro.shytyi@6wind.com>

--- pathd/path_zebra.c
+++ pathd/path_zebra.c
@@ -518,12 +518,14 @@ static void path_zebra_add_sr_policy_internal(struct srte_policy *policy,
 		zp.segment_list.nexthop_resolved_num = nhtd->nh_num;
 	}
 	if (znh && !sid_zero_ipv6(&policy->srv6_binding_sid) && segment_list &&
-	    zp.segment_list.nexthop_resolved_num)
+	    zp.segment_list.nexthop_resolved_num) {
 		(void)path_zebra_send_bsid(&policy->srv6_binding_sid, znh->ifindex,
 					   ZEBRA_SEG6_LOCAL_ACTION_END_B6_ENCAP,
 					   &zp.segment_list.srv6_segs.segs[0],
 					   zp.segment_list.srv6_segs.num_segs);
 
+		SET_FLAG(policy->flags, F_POLICY_BSID_IPV6_INSTALLED);
+	}
 
 	(void)zebra_send_sr_policy(pathd_zclient, ZEBRA_SR_POLICY_SET, &zp);
 }
