pguibert6WIND/frr

commit 2c41ef8c1798540c5639f6a2508e72c861d3bede

zebra: special handling for proto-NHG-based routes
For now let's assume proto-NHG-based routes are good to go
(we assume they are onlink/interface based anyway) and bypass
route resolution altogether.

Once we determine how to handle recursive nexthop-resolution for
proto-NHGs we will revisit this.

Signed-off-by: Stephen Worley <sworley@cumulusnetworks.com>

zebra/zebra_nhg.c
@@ -2291,6 +2291,22 @@ static uint32_t nexthop_list_active_update(struct route_node *rn,
 	return counter;
 }
 
+
+static uint32_t proto_nhg_nexthop_active_update(struct nexthop_group *nhg)
+{
+	struct nexthop *nh;
+	uint32_t curr_active = 0;
+
+	/* Assume all active for now */
+
+	for (nh = nhg->nexthop; nh; nh = nh->next) {
+		SET_FLAG(nh->flags, NEXTHOP_FLAG_ACTIVE);
+		curr_active++;
+	}
+
+	return curr_active;
+}
+
 /*
  * Iterate over all nexthops of the given RIB entry and refresh their
  * ACTIVE flag.  If any nexthop is found to toggle the ACTIVE flag,
@@ -2302,6 +2318,9 @@ int nexthop_active_update(struct route_node *rn, struct route_entry *re)
 {
 	struct nhg_hash_entry *curr_nhe;
 	uint32_t curr_active = 0, backup_active = 0;
+
+	if (re->nhe->id >= zclient_get_nhg_lower_bound())
+		return proto_nhg_nexthop_active_update(&re->nhe->nhg);
 
 	afi_t rt_afi = family2afi(rn->p.family);
 

commit 023e3bf874ac82306d6987f6feb529e7025f6f62

zebra: fix nexthop-group state inactive
When a route is attached to a nexthop-group that has a non directly
connected nexthop and has no allow-recursion flag, the route is
wrongly marked as active.
For example:

> interface loop1
>  ip address 192.168.0.1/24
> exit
> ip route 10.200.0.0/24 192.168.0.11
> nexthop-group ABCD
>  nexthop 10.200.0.77
> exit
> sharp install route 6.6.6.6 nexthop-group ABCD 1
> sharp install route 6.6.7.6 nexthop 10.200.0.78 1 no-recurse

The route to 6.6.6.6 is not inactive, contrary to the route to
6.6.7.6.

> # show ip route 6.6.6.6 nexthop-group
> Routing entry for 6.6.6.6/32
>   Known via "sharp", distance 150, metric 0, best
>   Last update 00:00:14 ago
>   Nexthop Group ID: 181818168
>     10.200.0.77, weight 1   <---- should be marked as inactive
>
> # show ip route 6.6.7.6 nexthop-group
> Routing entry for 6.6.7.6/32
>   Known via "sharp", distance 150, metric 0
>   Last update 00:00:10 ago
>   Nexthop Group ID: 1007
>     10.200.0.78 inactive, weight 1

The cause is that the sharpd nexthop-group is wrongly active

> # show nexthop-group rib 181818168
> ID: 181818168 (sharp)
>      RefCnt: 2
>      Uptime: 00:01:25
>      VRF: default
>      Depends: (1006)
>            via 10.200.0.77 (vrf default), weight 1
> # show nexthop-group rib 1007
> ID: 1007 (zebra)
>      RefCnt: 1
>      Uptime: 00:01:17
>      VRF: default
>            via 10.200.0.78 (vrf default) inactive, weight 1
>

Process all nexthop-groups in the same way in both situations.

Fixes: 2c41ef8c1798 ("zebra: special handling for proto-NHG-based routes")
Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

zebra/zebra_nhg.c
@@ -2862,21 +2862,6 @@ static uint32_t nexthop_list_active_update(struct route_node *rn,
 }
 
 
-static uint32_t proto_nhg_nexthop_active_update(struct nexthop_group *nhg)
-{
-	struct nexthop *nh;
-	uint32_t curr_active = 0;
-
-	/* Assume all active for now */
-
-	for (nh = nhg->nexthop; nh; nh = nh->next) {
-		SET_FLAG(nh->flags, NEXTHOP_FLAG_ACTIVE);
-		curr_active++;
-	}
-
-	return curr_active;
-}
-
 static uint32_t nexthop_active_update_common(struct route_node *rn,
 					     struct route_entry *re,
 					     struct nhg_hash_entry *curr_nhe)
@@ -2947,7 +2932,7 @@ int nexthop_active_update(struct route_node *rn, struct route_entry *re)
 	uint32_t curr_active;
 
 	if (PROTO_OWNED(re->nhe))
-		return proto_nhg_nexthop_active_update(&re->nhe->nhg);
+		return nexthop_active_update_common(rn, re, re->nhe);
 
 	/* Make a local copy of the existing nhe, so we don't work on/modify
 	 * the shared nhe.