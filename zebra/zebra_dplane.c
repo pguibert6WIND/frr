b74dbc02d4d410e10d06057f4674eb46bfff465e
Philippe Guibert <philippe.guibert@6wind.com>
2024-10-21 14:58:02 +0200

    zebra: add dataplane statistics about pic contexts updates
    There is no PIC statistics about the number of dataplane
    messages sent to the dataplane, be it FPM or KERNEL.
    
    Add statistics for pic contexts. Display it.
    
    > r1# show zebra dplane
    > Zebra dataplane:
    > Route updates:            49
    > Route update errors:      0
    > Nexthop updates:          14
    > Nexthop update errors:    0
    > PIC Nexthop updates:      15
    > PIC Nexthop update errors:0
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zebra_dplane.c
+++ zebra/zebra_dplane.c
@@ -580,6 +580,9 @@ static struct zebra_dplane_globals {
 	_Atomic uint32_t dg_nexthops_in;
 	_Atomic uint32_t dg_nexthop_errors;
 
+	_Atomic uint32_t dg_pic_nexthops_in;
+	_Atomic uint32_t dg_pic_nexthop_errors;
+
 	_Atomic uint32_t dg_lsps_in;
 	_Atomic uint32_t dg_lsp_errors;
 
@@ -4519,9 +4522,13 @@ dplane_nexthop_update_internal(struct nhg_hash_entry *nhe, enum dplane_op_e op)
 			SET_FLAG(nhe->flags, NEXTHOP_GROUP_INSTALLED);
 
 			dplane_ctx_free(&ctx);
-			atomic_fetch_add_explicit(&zdplane_info.dg_nexthops_in,
-						  1, memory_order_relaxed);
-
+			if (op == DPLANE_OP_PIC_NH_INSTALL || op == DPLANE_OP_PIC_NH_UPDATE ||
+			    op == DPLANE_OP_PIC_NH_DELETE)
+				atomic_fetch_add_explicit(&zdplane_info.dg_pic_nexthops_in,
+							  1, memory_order_relaxed);
+			else
+				atomic_fetch_add_explicit(&zdplane_info.dg_nexthops_in,
+							  1, memory_order_relaxed);
 			return ZEBRA_DPLANE_REQUEST_SUCCESS;
 		}
 
@@ -4529,14 +4536,24 @@ dplane_nexthop_update_internal(struct nhg_hash_entry *nhe, enum dplane_op_e op)
 	}
 
 	/* Update counter */
-	atomic_fetch_add_explicit(&zdplane_info.dg_nexthops_in, 1,
+	if (op == DPLANE_OP_PIC_NH_INSTALL || op == DPLANE_OP_PIC_NH_UPDATE ||
+            op == DPLANE_OP_PIC_NH_DELETE)
+		atomic_fetch_add_explicit(&zdplane_info.dg_pic_nexthops_in,
+					  1, memory_order_relaxed);
+	else
+		atomic_fetch_add_explicit(&zdplane_info.dg_nexthops_in, 1,
 				  memory_order_relaxed);
 
 	if (ret == AOK)
 		result = ZEBRA_DPLANE_REQUEST_QUEUED;
 	else {
-		atomic_fetch_add_explicit(&zdplane_info.dg_nexthop_errors, 1,
-					  memory_order_relaxed);
+		if (op == DPLANE_OP_PIC_NH_INSTALL || op == DPLANE_OP_PIC_NH_UPDATE ||
+		    op == DPLANE_OP_PIC_NH_DELETE)
+			atomic_fetch_add_explicit(&zdplane_info.dg_pic_nexthop_errors, 1,
+						  memory_order_relaxed);
+		else
+			atomic_fetch_add_explicit(&zdplane_info.dg_nexthop_errors, 1,
+						  memory_order_relaxed);
 		if (ctx)
 			dplane_ctx_free(&ctx);
 	}
@@ -6052,6 +6069,13 @@ int dplane_show_helper(struct vty *vty, bool detailed)
 	vty_out(vty, "Nexthop updates:          %" PRIu64 "\n", incoming);
 	vty_out(vty, "Nexthop update errors:    %" PRIu64 "\n", errs);
 
+	incoming = atomic_load_explicit(&zdplane_info.dg_pic_nexthops_in,
+					memory_order_relaxed);
+	errs = atomic_load_explicit(&zdplane_info.dg_pic_nexthop_errors,
+				    memory_order_relaxed);
+	vty_out(vty, "PIC Nexthop updates:          %" PRIu64 "\n", incoming);
+	vty_out(vty, "PIC Nexthop update errors:    %" PRIu64 "\n", errs);
+
 	vty_out(vty, "Other errors       :      %"PRIu64"\n", other_errs);
 	vty_out(vty, "Route update queue limit: %"PRIu64"\n", limit);
 	vty_out(vty, "Route update queue depth: %"PRIu64"\n", queued);
@@ -6765,9 +6789,9 @@ static void kernel_dplane_log_detail(struct zebra_dplane_ctx *ctx)
 static void kernel_dplane_handle_result(struct zebra_dplane_ctx *ctx)
 {
 	enum zebra_dplane_result res = dplane_ctx_get_status(ctx);
+	enum dplane_op_e op = dplane_ctx_get_op(ctx);
 
 	switch (dplane_ctx_get_op(ctx)) {
-
 	case DPLANE_OP_ROUTE_INSTALL:
 	case DPLANE_OP_ROUTE_UPDATE:
 	case DPLANE_OP_ROUTE_DELETE:
@@ -6803,10 +6827,15 @@ static void kernel_dplane_handle_result(struct zebra_dplane_ctx *ctx)
 	case DPLANE_OP_PIC_NH_INSTALL:
 	case DPLANE_OP_PIC_NH_UPDATE:
 	case DPLANE_OP_PIC_NH_DELETE:
-		if (res != ZEBRA_DPLANE_REQUEST_SUCCESS)
-			atomic_fetch_add_explicit(
-				&zdplane_info.dg_nexthop_errors, 1,
-				memory_order_relaxed);
+		if (res != ZEBRA_DPLANE_REQUEST_SUCCESS) {
+			if (op == DPLANE_OP_PIC_NH_INSTALL || op == DPLANE_OP_PIC_NH_UPDATE
+			    || op == DPLANE_OP_PIC_NH_DELETE)
+				atomic_fetch_add_explicit(&zdplane_info.dg_pic_nexthop_errors, 1,
+							  memory_order_relaxed);
+			else
+				atomic_fetch_add_explicit(&zdplane_info.dg_nexthop_errors, 1,
+							  memory_order_relaxed);
+		}
 		break;
 
 	case DPLANE_OP_LSP_INSTALL:
