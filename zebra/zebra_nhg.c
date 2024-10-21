d39422844364f7abad8bf1e60a805257bee32c5d
Philippe Guibert <philippe.guibert@6wind.com>
2024-10-21 10:44:22 +0200

    zebra: unlock fpm pic usage for all kind of nexthops
    Do permit usage of fpm nexthop pic for all kinds of nexthops.
    
    Signed-off-by: Philippe Guibert <philippe.guibert@6wind.com>

--- zebra/zebra_nhg.c
+++ zebra/zebra_nhg.c
@@ -701,8 +701,12 @@ static bool zebra_need_to_create_pic(struct nexthop *nh)
 {
 	if (!fpm_pic_nexthop)
 		return false;
-	if (nh && nh->nh_srv6 && nh->nh_srv6->seg6_segs && !sid_zero(nh->nh_srv6->seg6_segs))
-		return true;
+	if (nh) {
+		if (nh->nh_srv6 && nh->nh_srv6->seg6_segs && !sid_zero(nh->nh_srv6->seg6_segs))
+			return true;
+		if (nh->nh_label)
+			return true;
+	}
 	return false;
 }
 
