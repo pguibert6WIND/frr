96b9f36e23c35febb107b525a074fd2fc56b9b38
Loïc Sang <loic.sang@6wind.com>
2025-12-04 14:38:09 +0100

    reformat l2vpn pw flag
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>
    (cherry picked from commit d8311e49fa955ee62f24e961936d756c8b3ea59a)
    Signed-off-by: Loïc Sang <loic.sang@6wind.com>

--- lib/l2vpn.h
+++ lib/l2vpn.h
@@ -64,19 +64,22 @@ struct l2vpn_pw {
 RB_HEAD(l2vpn_pw_head, l2vpn_pw);
 RB_PROTOTYPE(l2vpn_pw_head, l2vpn_pw, entry, l2vpn_pw_compare);
 DECLARE_QOBJ_TYPE(l2vpn_pw);
-#define F_PW_STATUSTLV_CONF	0x01	/* status tlv configured */
-#define F_PW_STATUSTLV		0x02	/* status tlv negotiated */
-#define F_PW_CWORD_CONF		0x04	/* control word configured */
-#define F_PW_CWORD		0x08	/* control word negotiated */
-#define F_PW_STATIC_NBR_ADDR	0x10	/* static neighbor address configured */
-#define F_PW_SEND_REMOTE	0x20	/* send pw message to remote */
-#define F_PW_EVPN_NBR_ADDR   0x40       /* EVPN neighbor configured */
 
-#define F_PW_NO_ERR             0x00	/* no error reported */
-#define F_PW_LOCAL_NOT_FWD      0x01	/* locally can't forward over PW */
-#define F_PW_REMOTE_NOT_FWD     0x02	/* remote end of PW reported fwd error*/
-#define F_PW_NO_REMOTE_LABEL    0x03	/* have not recvd label from peer */
-#define F_PW_MTU_MISMATCH       0x04	/* mtu mismatch between peers */
+/* PW flags */
+#define F_PW_STATUSTLV_CONF  (1 << 0) /* status tlv configured */
+#define F_PW_STATUSTLV       (1 << 1) /* status tlv negotiated */
+#define F_PW_CWORD_CONF      (1 << 2) /* control word configured */
+#define F_PW_CWORD           (1 << 3) /* control word negotiated */
+#define F_PW_STATIC_NBR_ADDR (1 << 4) /* static neighbor address configured */
+#define F_PW_SEND_REMOTE     (1 << 5) /* send pw message to remote */
+#define F_PW_EVPN_NBR_ADDR   (1 << 6) /* EVPN neighbor configured */
+
+/* PW status code */
+#define F_PW_NO_ERR          (1 << 0) /* no error reported */
+#define F_PW_LOCAL_NOT_FWD   (1 << 1) /* locally can't forward over PW */
+#define F_PW_REMOTE_NOT_FWD  (1 << 2) /* remote end of PW reported fwd error*/
+#define F_PW_NO_REMOTE_LABEL (1 << 3) /* have not recvd label from peer */
+#define F_PW_MTU_MISMATCH    (1 << 4) /* mtu mismatch between peers */
 
 
 typedef enum { L2VPN_TYPE_VPWS = 1, L2VPN_TYPE_VPLS = 2 } l2vpn_types;
